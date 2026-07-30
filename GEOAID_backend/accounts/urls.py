a4ae8440ab6b211ae7ee6734703470a2edb8fb1d
ashleeapao <ashapao12@gmail.com>
2026-07-30 17:05:45 +0800

    Update GeoAid frontend and backend

--- GEOAID_backend/accounts/urls.py
+++ GEOAID_backend/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_user, cswd_dashboard
+from .views import login_user, cswd_dashboard, purok_dashboard
 
 urlpatterns = [
-    path('login/', login_user, name='login'),
-     path("cswd/dashboard/", cswd_dashboard),
+    path("login/", login_user, name="login"),
+    path("cswd/dashboard/", cswd_dashboard),
+    path("purok/dashboard/", purok_dashboard),
 ]
